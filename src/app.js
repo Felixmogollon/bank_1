8f5f3df711df09e31f4f671a811298c72b113273
Felixmogollon <felixmogollon45@gmail.com>
2024-01-07 15:00:20 -0500

    first commit

--- src/app.js
+++ src/app.js
@@ -1,9 +1,12 @@
 import express from "express";
 import { router } from "./routes/index.js";
+// este archivo configura una aplicación Express básica con middleware para manejar datos en formato 
+//JSON y URL-encoded, y establece rutas prefijadas para la versión 1 de la API ("/api/v1").
 
 const app = express();
 
 //middlewares para aceptar cuerpos en json y url-encoded
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
